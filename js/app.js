04f94b635f6e9d4e5e40fbbbf065f00141df0e68
olagz <olagodavivid@gmail.com>
2016-08-02 16:19:26 +0300

    updates

---
+++ js/app.js
@@ -0,0 +1,55 @@
+
+(function($){
+
+ $.fn.tabbed_interface = function(options) {
+
+  var defaults = {
+   fade_duration: 500,
+   
+   
+  };
+
+  var options = $.extend(defaults, options);
+
+  return this.each(function(el) {
+
+   el = this;
+
+   $(el).children('ul').children('li').children('a').click(function(){
+
+    $(el).children('div').fadeOut(options.fade_duration/2).
+     filter(this.hash).fadeIn(options.fade_duration/2);
+
+    return false;
+
+   });
+						
+   $('.box-content>a').bind("click touch", function(e)
+   
+   { e.preventDefault(); 
+	  
+				$(el).children('div').css('position','absolute').fadeOut(options.fade_duration/2);
+				
+       return false;
+		  
+	  });
+   
+  
+   $(el).children('div').css('position','absolute').hide();
+
+  
+  });
+
+ };
+})(jQuery);
+
+$(document).ready(function(){
+     $('#tabbed-interface').tabbed_interface({
+		
+								
+								fade_duration:1100
+								}); 
+     
+    });
+	
+
